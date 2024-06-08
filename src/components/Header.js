ed859804aa8108cdbcf715642500a47c1d4b462f
enjoroge <njrogeelizabeth53@gmail.com>
2024-06-08 23:31:14 +0300

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+// Header.js
+
+import React from 'react';
+
+function Header({ onDarkModeClick }) {
+  return (
+    <header>
+      <h1>My Shopping List</h1>
+      <button onClick={onDarkModeClick}>Toggle Dark Mode</button>
+    </header>
+  );
+}
+
+export default Header;
